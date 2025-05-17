b9506545c180625f750d60700276f6535e2e92c6
Victor Palmero <victor.palmero@undostres.com.mx>
2025-05-17 16:00:00 -0400

    feat: changes

--- src/main/java/com/palmerodev/harmoni_api/service/MyProfileService.java
+++ src/main/java/com/palmerodev/harmoni_api/service/MyProfileService.java
@@ -1,6 +1,7 @@
 package com.palmerodev.harmoni_api.service;
 
 import com.palmerodev.harmoni_api.model.request.SettingsRequest;
+import com.palmerodev.harmoni_api.model.request.UserInfoRequest;
 import com.palmerodev.harmoni_api.model.response.SettingsResponse;
 import com.palmerodev.harmoni_api.model.response.UserInfoResponse;
 import com.palmerodev.harmoni_api.model.response.ValidationResponse;
@@ -17,5 +18,8 @@ public interface MyProfileService {
 
     ValidationResponse validateName(String name);
 
-    UserInfoResponse updateUserProfile(UserInfoResponse userInfo);
+    UserInfoResponse updateUserProfile(UserInfoRequest userInfo);
+
+    boolean deleteUserProfile();
+
 }
