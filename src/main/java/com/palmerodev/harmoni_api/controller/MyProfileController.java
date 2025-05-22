6c40bbb7c0d37ed7378bf37fa181c59ab4e598d5
Victor Palmero <victor.palmero@undostres.com.mx>
2025-05-22 10:12:36 -0400

    feat: add my profile controller

---
+++ src/main/java/com/palmerodev/harmoni_api/controller/MyProfileController.java
@@ -0,0 +1,11 @@
+package com.palmerodev.harmoni_api.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/myProfile")
+@RequiredArgsConstructor
+public class MyProfileController {
+}
